6ef8e1d4f89bc33109dbacb648c93cef2785374f
hugo180 <hugorubio180@gmail.com>
2022-01-24 23:40:59 -0700

    Add files via upload
    added my files

---
+++ questions.js
@@ -0,0 +1,59 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Inside which HTML element do we put the JavaScript?",
+    answer: "script",
+    options: [
+      "script",
+      "js",
+      "javascript",
+      "scripting "
+    ]
+  },
+    {
+    numb: 2,
+    question: "Where is the correct place to insert a JavaScript? ",
+    answer: "Both the head section and the body section are correct",
+    options: [
+      "The body section",
+      "Both the head section and the body section are correct",
+      "The head section",
+      "none"
+    ]
+  },
+    {
+    numb: 3,
+    question: "The external JavaScript file must contain the script tag.",
+    answer: "false",
+    options: [
+      "true",
+      "false",
+      "i'm not sure",
+      "i prefer to not answer"
+    ]
+  },
+    {
+    numb: 4,
+    question: "How do you create a function in JavaScript?",
+    answer: "function myFunction()",
+    options: [
+      "function = myFunction()",
+      "function myFunction()",
+      "function:myFunction()",
+      "none"
+    ]
+  },
+    {
+    numb: 5,
+    question: "How do you call a function named myFunction?",
+    answer: "myFunction()",
+    options: [
+      "myFunction()",
+      "call function myFunction()",
+      "call myFunction()",
+      "none"
+    ]
+  },
+
+];
